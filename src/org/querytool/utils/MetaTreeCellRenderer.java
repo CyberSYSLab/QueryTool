53bb1eaa9c7bf1e4b02683e5bd9c1f418e6a10de
Vladimir V. <avega.dev@yandex.ru>
2011-11-04 13:24:59 +0300

    New icons for db and table tree nodes

--- src/org/querytool/utils/MetaTreeCellRenderer.java
+++ src/org/querytool/utils/MetaTreeCellRenderer.java
@@ -22,11 +22,28 @@ package org.querytool.utils;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
+import java.awt.Toolkit;
+import java.net.URL;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JTree;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.DefaultTreeCellRenderer;
 
 public class MetaTreeCellRenderer extends DefaultTreeCellRenderer {
+
+    private Icon dbIcon;
+    private Icon tableIcon;
+    
+    public MetaTreeCellRenderer() {
+        Toolkit kit = Toolkit.getDefaultToolkit();
+        URL url;
+
+        url = ClassLoader.getSystemResource("resources/database-leaf.png");
+        dbIcon = new ImageIcon(kit.createImage(url));
+        url = ClassLoader.getSystemResource("resources/table-leaf.png");
+        tableIcon = new ImageIcon(kit.createImage(url));
+    }
 
     private String highLightName = "";
     
@@ -38,6 +55,8 @@ public class MetaTreeCellRenderer extends DefaultTreeCellRenderer {
     public Component getTreeCellRendererComponent(JTree tree, Object value, boolean selected, boolean expanded, boolean leaf, int row, boolean hasFocus) {
         super.getTreeCellRendererComponent(tree, value, selected, expanded, leaf, row, hasFocus);
 
+        int level = ((DefaultMutableTreeNode)value).getLevel();
+        
         if (value != null) {
             if (value instanceof DefaultMutableTreeNode) {
                 if (value.toString().equalsIgnoreCase(highLightName)) {
@@ -47,6 +66,18 @@ public class MetaTreeCellRenderer extends DefaultTreeCellRenderer {
                     setForeground(Color.black);
                     setFont(getFont().deriveFont(Font.PLAIN));
                 }
+            }
+            if (level == 1) {
+                setOpenIcon(dbIcon);
+                setClosedIcon(dbIcon);
+                setIcon(dbIcon);
+                if (leaf) setLeafIcon(dbIcon);
+            }
+            if (level == 2) {
+                setOpenIcon(tableIcon);
+                setClosedIcon(tableIcon);
+                setLeafIcon(tableIcon);
+                setIcon(tableIcon);
             }
         }
         return this;
